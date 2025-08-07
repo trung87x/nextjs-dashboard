5e5512906cce48275cc91047028c734fc69cc267
trung87x <job.dinhquangtrung@gmail.com>
2025-08-07 21:14:07 +0700

    .
    .

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,9 +32,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    console.log('Fetching latest invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
